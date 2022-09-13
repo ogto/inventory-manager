660f3c289e19ce48a0626ae11d37d15e4531fabc
GayeongLee21 <s3891958@rmit.edu.vn>
2022-09-13 19:33:44 +0900

    [fix] menu bug edit

--- inc/menu.php
+++ inc/menu.php
@@ -4,9 +4,9 @@
 <div id="menu">
     <ul>
         <li><a href="products.php" <?php if($currentPage == 'products') echo 'class="active"'; ?>>Current Inventory</a></li>
-        <li><a href="incoming-purchases.php" <?php if($_GET['currentPage'] == 'incoming' ?? null) echo 'class="active"'; ?>>Incoming Purchases</a></li>
-        <li><a href="outgoing-order.php" <?php if($_GET['currentPage'] == 'outgoing' ?? null) echo 'class="active"'; ?>>Outgoing Orders</a></li>
-        <li><a href="reports.php" <?php if($_GET['currentPage'] == 'reports' ?? null) echo 'class="active"'; ?>>Reports</a></li>
-        <li><a href="barcodes.php" <?php if($_GET['currentPage'] == 'barcodes' ?? null) echo 'class="active"'; ?>>Barcodes</a></li>
+        <li><a href="incoming-purchases.php" <?php if($currentPage == 'incoming') echo 'class="active"'; ?>>Incoming Purchases</a></li>
+        <li><a href="outgoing-order.php" <?php if($currentPage== 'outgoing') echo 'class="active"'; ?>>Outgoing Orders</a></li>
+        <li><a href="reports.php" <?php if($currentPage == 'reports') echo 'class="active"'; ?>>Reports</a></li>
+        <li><a href="barcodes.php" <?php if($currentPage == 'barcodes') echo 'class="active"'; ?>>Barcodes</a></li>
     </ul>
 </div>
