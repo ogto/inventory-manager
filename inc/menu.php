1f117bb1cea6da99878f644f73532bccedb3a4cf
GayeongLee21 <s3891958@rmit.edu.vn>
2022-09-13 19:32:01 +0900

    [fix] variable

--- inc/menu.php
+++ inc/menu.php
@@ -1,6 +1,9 @@
+<?php
+    $currentPage = $_GET['currentPage'] ?? null;
+?>
 <div id="menu">
     <ul>
-        <li><a href="products.php" <?php if($_GET['currentPage'] == 'products' ?? null) echo 'class="active"'; ?>>Current Inventory</a></li>
+        <li><a href="products.php" <?php if($currentPage == 'products') echo 'class="active"'; ?>>Current Inventory</a></li>
         <li><a href="incoming-purchases.php" <?php if($_GET['currentPage'] == 'incoming' ?? null) echo 'class="active"'; ?>>Incoming Purchases</a></li>
         <li><a href="outgoing-order.php" <?php if($_GET['currentPage'] == 'outgoing' ?? null) echo 'class="active"'; ?>>Outgoing Orders</a></li>
         <li><a href="reports.php" <?php if($_GET['currentPage'] == 'reports' ?? null) echo 'class="active"'; ?>>Reports</a></li>
