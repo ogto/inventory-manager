79f00a2770b8807d2cad42d5b8f58b66bab84d1b
GayeongLee21 <s3891958@rmit.edu.vn>
2022-09-13 17:58:24 +0900

    first commit

---
+++ reports.php
@@ -0,0 +1,65 @@
+<?php
+use phpGrid\C_DataGrid;
+
+include_once("phpGrid/conf.php");
+require_once("phpChart/conf.php");
+include_once('inc/head.php');
+?>
+
+<h1>My Inventory Manager Reports</h1>
+
+<?php
+$_GET['currentPage'] = 'reports';
+include_once('inc/menu.php');
+?>
+
+<br />
+<div id="label_info"></div>
+
+
+<?php
+$dgProd = new C_DataGrid('SELECT id, ProductLabel, InventoryReceived FROM products', 'id', 'products');
+$dgProd->set_col_hidden('id', false);
+$dgProd->set_dimension('auto', 'auto')->set_pagesize(100);
+$onGridLoadComplete = <<<ONGRIDLOADCOMPLETE
+function(status, rowid)
+{
+	var dataX = [];
+	var dataY = [];
+
+	d1 = $('#products').jqGrid('getCol', 'ProductLabel', false);
+	d2 = $('#products').jqGrid('getCol', 'InventoryReceived', false);
+	
+	npoints = d1.length;
+	for(var i=0; i < npoints; i++){
+		dataX[i] = [i+1, d1[i]];
+		dataY[i] = [i+1, parseInt(d2[i])];
+	}
+
+    var pieData = [];
+    for(var j=0; j < dataX.length; j++)
+    {
+        pieData.push([dataX[j][1], dataY[j][1]]);
+    }
+    console.log(pieData);
+    _PieChart.series[0].data = pieData;
+    _PieChart.replot({resetAxes:true});
+}
+ONGRIDLOADCOMPLETE;
+$dgProd->add_event("jqGridLoadComplete", $onGridLoadComplete);
+$dgProd->display();
+?>
+
+<style>
+div#resizable,
+div#gbox_products{
+  float: left;
+}
+#label_info{
+  color:green;
+}
+</style>
+
+<?php
+include_once('inc/footer.php');
+?>
