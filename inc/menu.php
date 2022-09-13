8cdb8754f8adcc0ee5497981ed76885faaee2148
GayeongLee21 <s3891958@rmit.edu.vn>
2022-09-13 21:29:40 +0900

    [fix] barcode remove

--- inc/menu.php
+++ inc/menu.php
@@ -7,6 +7,6 @@
         <li><a href="incoming-purchases.php" <?php if($currentPage == 'incoming') echo 'class="active"'; ?>>Incoming Purchases</a></li>
         <li><a href="outgoing-order.php" <?php if($currentPage== 'outgoing') echo 'class="active"'; ?>>Outgoing Orders</a></li>
         <li><a href="reports.php" <?php if($currentPage == 'reports') echo 'class="active"'; ?>>Reports</a></li>
-        <li><a href="barcodes.php" <?php if($currentPage == 'barcodes') echo 'class="active"'; ?>>Barcodes</a></li>
+        <!-- <li><a href="barcodes.php" <?php if($currentPage == 'barcodes') echo 'class="active"'; ?>>Barcodes</a></li> -->
     </ul>
 </div>
